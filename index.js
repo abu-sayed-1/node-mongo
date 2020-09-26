1ee6c8fca59b927beaf65f293fe160cda51ee62a
as ahmed <rakibasrr13@gmail.com>
2020-09-26 16:33:48 +0300

    node mongo

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+ 
+
+const app = express();
+
+
+app.get('/',(req, res ) => {
+    res.send('thanks you very much rakid khan kjfslakfdlakj');
+})
+app.listen(3100, () => console.log("rakib khan3100"));
